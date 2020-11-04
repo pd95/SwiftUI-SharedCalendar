3f3ba8920e32703f76c66f79623961818ae3b609
Philipp <pd95@users.noreply.github.com>
2020-11-04 21:24:13 +0100

    Initial Commit

---
+++ SharedCalendar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SharedCalendar
+//
+//  Created by Philipp on 04.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
